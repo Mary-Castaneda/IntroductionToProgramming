1232aee7f64cda416ad75e9f9144892909dcac55
Mary Obray <MaryObray@gmail.com>
2017-01-05 21:55:29 -0500

    all prep exercises

---
+++ prepfile3.rb
@@ -0,0 +1,6 @@
+movies = {:Elf => "2003", :Lord_of_the_Rings1 => "2001", :Tangled => "2010", :Up => "2009", :The_Incredibles => "2004"}
+puts movies[:Elf]
+puts movies[:Lord_of_the_Rings1]
+puts movies[:Tangled]
+puts movies[:Up]
+puts movies[:The_Incredibles]
---
+++ prepfile4.rb
@@ -0,0 +1,6 @@
+movies = [2003, 2001, 2010, 2009, 2004]
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
